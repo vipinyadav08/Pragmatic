ef61c26ff7a721dd524992442a6c1742833c218f
Vipin Kumar Yadav <er.vipinyadav@gmail.com>
2014-05-07 21:15:03 +0530

    Change MessageBox to ModernDialog Message.

--- Source/Pragmatic.Example.Client.Desktop/UserInteraction.cs
+++ Source/Pragmatic.Example.Client.Desktop/UserInteraction.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Linq;
 using System.Windows;
+using FirstFloor.ModernUI.Windows.Controls;
 using Pragmatic.Interaction;
 
 namespace Pragmatic.Example.Client.Desktop
@@ -10,58 +11,51 @@ namespace Pragmatic.Example.Client.Desktop
     {
         internal static void ShowError(string message, Response response)
         {
-            MessageBox.Show(string.Format("{1}{0}{2}",
+            ModernDialog.ShowMessage(string.Format("{1}{0}{2}",
                                 Environment.NewLine,
                                 message,
                                 response.Errors.Aggregate(string.Empty,
                                     (result, error) => string.Format("{1}{2}{0}", Environment.NewLine, result, error.Message))),
                              "Error",
-                             MessageBoxButton.OK,
-                             MessageBoxImage.Error);
+                             MessageBoxButton.OK);
         }
 
         internal static void ShowInformation(string message)
         {
-            MessageBox.Show(message,
+            ModernDialog.ShowMessage(message,
                             "Information",
-                            MessageBoxButton.OK,
-                            MessageBoxImage.Information);
+                            MessageBoxButton.OK);
         }
 
         internal static MessageBoxResult ShowQuestion(string message, MessageBoxButton messageBoxButton)
         {
-           return  MessageBox.Show(message,
+            return ModernDialog.ShowMessage(message,
                                    "Question",
-                                   messageBoxButton,
-                                   MessageBoxImage.Question);
+                                   messageBoxButton);
         }
 
         internal static MessageBoxResult ShowResponse(string message, Response response, MessageBoxButton messageBoxButton)
         {
-            MessageBoxImage messageBoxImage = MessageBoxImage.Information;
-            string caption = "Information";
+            var caption = "Information";
 
             if (response.HasErrors)
             {
-                messageBoxImage = MessageBoxImage.Error;
                 caption = "Error";
             }
             else if (response.HasWarnings)
             {
-                messageBoxImage = MessageBoxImage.Warning;
                 caption = "Warning";
             }
             else if (response.HasInformation)
             {
-                messageBoxImage = MessageBoxImage.Information;
                 caption = "Information";
             }
 
-            string responseMessage = (response.Errors.Aggregate(string.Empty, (result, error) => string.Format("{1}{2}{0}", Environment.NewLine, result, error.Message)) + Environment.NewLine +
+            var responseMessage = (response.Errors.Aggregate(string.Empty, (result, error) => string.Format("{1}{2}{0}", Environment.NewLine, result, error.Message)) + Environment.NewLine +
                                       response.Warnings.Aggregate(string.Empty, (result, error) => string.Format("{1}{2}{0}", Environment.NewLine, result, error.Message)) + Environment.NewLine +
                                       response.Information.Aggregate(string.Empty, (result, error) => string.Format("{1}{2}{0}", Environment.NewLine, result, error.Message))).Trim();
 
-            return MessageBox.Show((message + Environment.NewLine + responseMessage).Trim(), caption, messageBoxButton, messageBoxImage);
+            return ModernDialog.ShowMessage((message + Environment.NewLine + responseMessage).Trim(), caption, messageBoxButton);
         }
     }
 }
